c11454cf00c09e8f2121c1db3809526504f36913
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-03 14:15:36 +0700

    [UPDATE] WIP FaceDetection model

--- lipster-mobile/model/FaceDetection.swift
+++ lipster-mobile/model/FaceDetection.swift
@@ -1,1 +1,23 @@
-import Foundation
+import UIKit
+import Vision
+
+class FaceDetection {
+    open func getLipsLandmarks(for source: UIImage, complete: @escaping (String) -> Void) {
+        let faceDetectRequest = VNDetectFaceLandmarksRequest { (request, error) in
+            if error == nil {
+                if let results = request.results as? [VNFaceObservation] {
+                    for faceObservation in results {
+                        guard let landmarks = faceObservation.landmarks else {
+                            continue
+                        }
+                        var innerLip = landmarks.innerLips
+                        var outerLip = landmarks.outerLips
+                        
+                        
+                    }
+                }
+            }
+            complete("#ffffff")
+        }
+    }
+}
