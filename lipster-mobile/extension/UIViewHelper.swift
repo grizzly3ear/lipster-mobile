37695d06afddfbcbf9d1e1018315c993a1e86f88
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-05 14:34:22 +0700

    [UPDATE] move extension for UIKit to extension folder

---
+++ lipster-mobile/extension/UIViewHelper.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
