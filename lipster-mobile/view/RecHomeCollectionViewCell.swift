c02452f366fabc1add4f2c87cc5a4d9d18d27f05
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-04-19 18:23:43 +0700

    [UPDATE] create Recommended CollectionViewCell on storyBoard

---
+++ lipster-mobile/view/RecHomeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecHomeCollectionViewCell.swift
+//  lipster-mobile
+//
+//  Created by Mainatvara on 19/4/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class RecHomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recImageView: UIImageView!
+    @IBOutlet weak var recBrandLabel: UILabel!
+    @IBOutlet weak var recNameLabel: UILabel!
+}
---
+++ lipster-mobile/view/RecommendCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendCollectionViewCell.swift
+//  lipster-mobile
+//
+//  Created by Mainatvara on 19/4/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recLipImageView: UIImageView!
+    @IBOutlet weak var recLipBrandLabel: UILabel!
+    @IBOutlet weak var recLipNameLabel: UILabel!
+}
