ab460bcb3493f9ccb51815b7cd2ca8c659cc64ba
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-20 16:21:22 +0700

    [UPDATE] convert SwiftSpinner to swift 5

--- Pods/SwiftSpinner/SwiftSpinner/SwiftSpinner.swift
+++ Pods/SwiftSpinner/SwiftSpinner/SwiftSpinner.swift
@@ -220,7 +220,7 @@ public class SwiftSpinner: UIView {
         let token = UUID().uuidString
         delayedTokens.append(token)
         SwiftSpinner.sharedInstance.delay(delay, completion: {
-            if let index = delayedTokens.index(of: token) {
+            if let index = delayedTokens.firstIndex(of: token) {
                 delayedTokens.remove(at: index)
                 SwiftSpinner.show(title, animated: animated)
             }
