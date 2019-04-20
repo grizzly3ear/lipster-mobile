8f1b9859ae99f29ad256535bab7d54e17199a287
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-04-20 13:43:30 +0700

    [UPDATE] update trendDetail in StoryBoard

---
+++ lipster-mobile/view/TrendsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrendsCollectionViewCell.swift
+//  lipster-mobile
+//
+//  Created by Mainatvara on 20/4/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class TrendsCollectionViewCell: UICollectionViewCell {
+    
+}
