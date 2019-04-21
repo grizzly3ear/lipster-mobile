dcadee6cbcd55ce84871ce825c04062b2b58e6c1
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-04-21 14:56:17 +0700

    [UPDATE] add extension in TryMeViewController

--- lipster-mobile/controller/TryMeViewController.swift
+++ lipster-mobile/controller/TryMeViewController.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-class TryMeViewController: UIViewController , UICollectionViewDataSource ,UICollectionViewDelegate {
+class TryMeViewController: UIViewController  {
  
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBAction func clickedLipColorBtn(_ sender: Any) {
+        print("clicked color button")
     }
+    
     let colorCode:[UIColor] = [UIColor(rgb: 0xB74447),UIColor(rgb: 0xFA4855),UIColor(rgb: 0xFE486B),UIColor(rgb: 0xFF9A94) ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
+}
+
+extension TryMeViewController : UICollectionViewDataSource ,UICollectionViewDelegate{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-      return  colorCode.count
-     
+        return  colorCode.count
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -34,6 +37,4 @@ class TryMeViewController: UIViewController , UICollectionViewDataSource ,UIColl
         return cell
     }
     
-   
-
 }
