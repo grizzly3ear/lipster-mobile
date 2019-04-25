6994c12b163052075f02fe34e197e0d15b7bc03e
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-25 14:43:38 +0700

    [ADD] Notification model

---
+++ lipster-mobile/model/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  lipster-mobile
+//
+//  Created by Bank on 25/4/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class Notification {
+    
+}
