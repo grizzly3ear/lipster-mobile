fd93f835d7fc303a7f030ac152a41029a910ef6b
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-25 15:31:33 +0700

    [ADD] HttpRequest  Model

---
+++ lipster-mobile/model/HttpRequest.swift
@@ -0,0 +1,14 @@
+//
+//  HttpRequest.swift
+//  lipster-mobile
+//
+//  Created by Bank on 25/4/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class HttpRequest {
+    
+}
