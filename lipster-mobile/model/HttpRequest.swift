eed08bd8a32220e2d9465c165a0a9863f663c57c
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-04-29 10:52:44 +0700

    [UPDATE] implement Alamofire, SwiftyJSON to model

--- lipster-mobile/model/HttpRequest.swift
+++ lipster-mobile/model/HttpRequest.swift
@@ -8,7 +8,89 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class HttpRequest {
     
+    var domain: String!
+    var token: String!
+    
+    init(_ domain: String) {
+        self.domain = domain
+        self.token = ""
+    }
+    
+    init(_ domain: String, _ token: String) {
+        self.domain = domain
+        self.token = token
+    }
+    
+    public func get(_ route: String, _ params: [String: Any]?, _ header: [String: String]?, completion: @escaping (JSON?) -> (Void)) {
+        
+        guard let url = URL(string: "\(domain)/\(route)") else {
+            completion(nil)
+            return
+        }
+        
+        Alamofire.request(url, method: .get, parameters: params, headers: header).validate().responseJSON { (response) in
+            
+            guard response.result.isSuccess else {
+                completion(nil)
+                return
+            }
+            
+            guard let json = try? JSON(response.data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(json)
+        }
+    }
+    
+    public func post(_ route: String, _ params: [String: Any]?, _ header: [String: String]?, completion: @escaping (JSON?) -> (Void)) {
+        
+        guard let url = URL(string: "\(domain)/\(route)") else {
+            completion(nil)
+            return
+        }
+        
+        Alamofire.request(url, method: .post, parameters: params, headers: header).validate().responseJSON { (response) in
+            
+            guard response.result.isSuccess else {
+                completion(nil)
+                return
+            }
+            
+            guard let json = try? JSON(response.data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(json)
+        }
+    }
+    
+    public func put(_ route: String, _ params: [String: Any]?, _ header: [String: String]?, completion: @escaping (JSON?) -> (Void)) {
+        
+        guard let url = URL(string: "\(domain)/\(route)") else {
+            completion(nil)
+            return
+        }
+        
+        Alamofire.request(url, method: .put, parameters: params, headers: header).validate().responseJSON { (response) in
+            
+            guard response.result.isSuccess else {
+                completion(nil)
+                return
+            }
+            
+            guard let json = try? JSON(response.data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(json)
+        }
+    }
 }
