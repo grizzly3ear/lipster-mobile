2448bf0aae38493efdf29d7bc99e145c1bf19c27
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-05-10 18:29:33 +0700

    [UPDATE] add nearby view

---
+++ lipster-mobile/controller/NearByViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NearByViewController.swift
+//  lipster-mobile
+//
+//  Created by Mainatvara on 10/5/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class NearByViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+
+}
