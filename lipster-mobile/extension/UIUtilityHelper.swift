9ac1431165ef632d4a1164049efed205c56e904c
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-05-26 02:01:43 +0700

    [ADD] UtilityHelper function

---
+++ lipster-mobile/extension/UIUtilityHelper.swift
@@ -0,0 +1,83 @@
+//
+//  StaticHelper.swift
+//  lipster-mobile
+//
+//  Created by Bank on 26/5/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+import FirebaseMLVision
+
+public class UIUtilityHelper {
+    
+    public static func imageOrientation(
+        fromDevicePosition devicePosition: AVCaptureDevice.Position = .back
+        ) -> UIImage.Orientation {
+        var deviceOrientation = UIDevice.current.orientation
+        if deviceOrientation == .faceDown || deviceOrientation == .faceUp ||
+            deviceOrientation == .unknown {
+            deviceOrientation = currentUIOrientation()
+        }
+        switch deviceOrientation {
+        case .portrait:
+            return devicePosition == .front ? .leftMirrored : .right
+        case .landscapeLeft:
+            return devicePosition == .front ? .downMirrored : .up
+        case .portraitUpsideDown:
+            return devicePosition == .front ? .rightMirrored : .left
+        case .landscapeRight:
+            return devicePosition == .front ? .upMirrored : .down
+        case .faceDown, .faceUp, .unknown:
+            return .up
+        }
+    }
+    
+    private static func currentUIOrientation() -> UIDeviceOrientation {
+        let deviceOrientation = { () -> UIDeviceOrientation in
+            switch UIApplication.shared.statusBarOrientation {
+            case .landscapeLeft:
+                return .landscapeRight
+            case .landscapeRight:
+                return .landscapeLeft
+            case .portraitUpsideDown:
+                return .portraitUpsideDown
+            case .portrait, .unknown:
+                return .portrait
+            }
+        }
+        guard Thread.isMainThread else {
+            var currentOrientation: UIDeviceOrientation = .portrait
+            DispatchQueue.main.sync {
+                currentOrientation = deviceOrientation()
+            }
+            return currentOrientation
+        }
+        return deviceOrientation()
+    }
+    
+    public static func visionImageOrientation(
+        from imageOrientation: UIImage.Orientation
+        ) -> VisionDetectorImageOrientation {
+        switch imageOrientation {
+        case .up:
+            return .topLeft
+        case .down:
+            return .bottomRight
+        case .left:
+            return .leftBottom
+        case .right:
+            return .rightTop
+        case .upMirrored:
+            return .topRight
+        case .downMirrored:
+            return .bottomLeft
+        case .leftMirrored:
+            return .leftTop
+        case .rightMirrored:
+            return .rightBottom
+        }
+    }
+}
