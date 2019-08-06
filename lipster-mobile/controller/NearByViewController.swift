e72282850f8f01e4c704267ea25cf4d540987bc5
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-08-06 19:26:40 +0700

    [ADD] init map to project

--- lipster-mobile/controller/NearByViewController.swift
+++ lipster-mobile/controller/NearByViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class NearByViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        self.locationManager.requestWhenInUseAuthorization()
+        initCoreLocationDelegate()
+        initMapViewDelegate()
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenter(coor, animated: true)
+        }
+    }
+
+}
+
+extension NearByViewController: CLLocationManagerDelegate {
+    func initCoreLocationDelegate() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
+
+extension NearByViewController: MKMapViewDelegate {
+    func initMapViewDelegate() {
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
     }
     
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "Javed Multani"
+        annotation.subtitle = "current location"
+        mapView.addAnnotation(annotation)
+    }
 }
