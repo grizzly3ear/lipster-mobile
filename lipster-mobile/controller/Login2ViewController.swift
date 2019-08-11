b53d90feb12dfcb5336c5da1b2418e25cb8e0464
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-08-11 22:28:45 +0700

    [ADD] add new login storyboard

---
+++ lipster-mobile/controller/Login2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Login2ViewController.swift
+//  lipster-mobile
+//
+//  Created by Mainatvara on 11/8/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class Login2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
