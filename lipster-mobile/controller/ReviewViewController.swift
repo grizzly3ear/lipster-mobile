349c492d476c586d04bde65434c608928121640d
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-09-16 16:07:13 +0700

    [UPDATE] update reviewTableView

--- lipster-mobile/controller/ReviewViewController.swift
+++ lipster-mobile/controller/ReviewViewController.swift
@@ -16,16 +16,47 @@ class ReviewViewController: UIViewController {
     @IBOutlet weak var lipstickName: UILabel!
     
     @IBOutlet weak var reviewTableView: UITableView!
-    @IBOutlet weak var typeReviewTextView: UITextView!
-    @IBOutlet weak var clickedPostButton: UIButton!
-    
-    var reviews: [UserReview] = [UserReview]()
-    var lipstick : Lipstick?
-    
+
+    @IBOutlet weak var typeReview: UITextField!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.userList = self.createUserArray()
         
+    }
+    @IBAction func sendButton(_ sender: Any) {
+        insertNewReview()
+        
+    }
+    
+    var userList  = [UserReview] ()
+    
+    func createUserArray() -> [UserReview] {
+        let user1 : UserReview = UserReview(userProfile: #imageLiteral(resourceName: "user2"), userReview: "REVIEWWWWWWWWWWWWWW!!!!!", userName: "BankAha Wisarut", dateReview: " 07 May 2019" )
+        let user2 : UserReview = UserReview(userProfile: #imageLiteral(resourceName: "user1"), userReview: "nice!!!!!!!!!", userName: "Bowie Ketsara", dateReview: "07 May 2019" )
+        
+        return [user1,user2]
+    }
+    var userReviews: [String] = ["nicccccceeeeeeeeeeeee",
+                                 "love this color , should try!",
+                                 "nice one."]
+    
+    func insertNewReview() {
+        
+        if typeReview.text!.isEmpty {
+            print("Add Review Text Field is empty")
+        }
+        //cell.lipNameLabel.text = lipList[indexPath.row].lipstickName
+        userReviews.append(typeReview.text!)
+        let indexPath = IndexPath(row: userReviews.count - 1, section: 0)
+        
+        reviewTableView.beginUpdates()
+        reviewTableView.insertRows(at: [indexPath], with: .automatic)
+        reviewTableView.endUpdates()
+        
+        typeReview.text = ""
+        view.endEditing(true)
     }
 
 }
@@ -33,15 +64,18 @@ class ReviewViewController: UIViewController {
 extension ReviewViewController: UITableViewDelegate , UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return reviews.count
+        return userReviews.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserReviewTableViewCell") as! UserReviewTableViewCell
-        cell.userReviewLabel.text = reviews[indexPath.item].userReview
-        cell.userNameLabel.text = reviews[indexPath.item].userName
-        cell.reviewDate.text = reviews[indexPath.item].dateReview
+        let review = userReviews[indexPath.row]
+        cell.userReviewLabel.text = review
+//        cell.userReviewLabel.text = reviews[indexPath.item].userReview
+//        cell.userNameLabel.text = reviews[indexPath.item].userName
+//        cell.reviewDate.text = reviews[indexPath.item].dateReview
+        
         
         return cell
     }
@@ -51,44 +85,6 @@ extension ReviewViewController: UITableViewDelegate , UITableViewDataSource {
     }
 }
 
-extension ReviewViewController: UITextViewDelegate {
-    
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.text == "Write your review here." {
-            textView.text = ""
-            textView.textColor = UIColor.black
-        }
-    }
-    
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder()
-        }
-        return true
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        if textView.text.trim() == "" {
-            clickedPostButton.isEnabled = false
-        } else {
-            clickedPostButton.isEnabled = true
-        }
-    }
-}
 
-// Post Button Config
-extension ReviewViewController {
-    @IBAction func clickedPostReviewButton(_ sender: Any) {
-//        
-//                userReviews.append(typeReviewTextView.text!)
-//                let indexPath = IndexPath(row: userReviews.count - 1, section: 0)
-//        
-//                reviewTableView.beginUpdates()
-//                reviewTableView.insertRows(at: [indexPath], with: .automatic)
-//                reviewTableView.endUpdates()
-//        
-//                typeReviewTextView.text = ""
-//                clickedPostButton.isEnabled = false
-//                view.endEditing(true)
-    }
-}
+
+
--- lipster-mobile/view/UserReviewTableViewCell.swift
+++ lipster-mobile/view/UserReviewTableViewCell.swift
@@ -24,15 +24,5 @@ class UserReviewTableViewCell: UITableViewCell {
     }
     
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
