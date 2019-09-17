afa304eb91b94ba5046655b63279f0e333094d07
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-09-17 16:10:32 +0700

    [UPDATE] update titleNavigation

--- lipster-mobile/controller/NewLipstickDetailViewcontroller.swift
+++ lipster-mobile/controller/NewLipstickDetailViewcontroller.swift
@@ -59,7 +59,7 @@ class NewLipstickDetailViewcontroller: UIViewController {
         initialUI()
         
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        //self.titleNavigationItem.title = lipstick?.lipstickBrand
+        self.titleNavigationItem.title = lipstick?.lipstickBrand
      
         segmentedControl3.append(title: "Description").set(title: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1) , for: .selected)
         segmentedControl3.append(title: "Ingredient").set(title: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: .selected)
