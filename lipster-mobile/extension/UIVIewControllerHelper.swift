69ed7d642bb2c7cdc272fea23cb3fc02a1e54c4e
Bank <udomphol2@gmail.com>
2019-09-25 12:34:12 +0700

    [ADD] tabbar show hide animation

---
+++ lipster-mobile/extension/UIVIewControllerHelper.swift
@@ -0,0 +1,49 @@
+//
+//  UIVIewController.swift
+//  lipster-mobile
+//
+//  Created by Vitsarut Udomphol on 25/9/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideTabBar(_ duration: Double = 0.5) {
+        var frame = self.tabBarController?.tabBar.frame
+        frame?.origin.y = self.view.frame.size.height + (self.tabBarController?.tabBar.frame.size.height)!
+        
+//        UIView.animate(withDuration: duration, animations: {
+//            self.tabBarController?.tabBar.frame = frame!
+//        })
+        
+        
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            usingSpringWithDamping: 1,
+            initialSpringVelocity: 0,
+            options: .curveLinear,
+            animations: {
+                self.tabBarController?.tabBar.frame = frame!
+            }
+        )
+    }
+    
+    func showTabBar(_ duration: Double = 0.3) {
+        var frame = self.tabBarController?.tabBar.frame
+        frame?.origin.y = self.view.frame.size.height + 15
+        
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            usingSpringWithDamping: 1,
+            initialSpringVelocity: 0,
+            options: .curveLinear,
+            animations: {
+                self.tabBarController?.tabBar.frame = frame!
+            }
+        )
+    }
+}
