d3e87798996b7a9c9734f11c17e556922f80f936
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-09-27 18:01:08 +0700

    [UPDATE] map func

--- lipster-mobile/controller/NearByViewController.swift
+++ lipster-mobile/controller/NearByViewController.swift
@@ -16,10 +16,10 @@ class NearByViewController: UIViewController   {
     
     var stores = [Store]()
     func createStoreArray() -> [Store] {
-        let store1 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora CentralPlaza Ladprao", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "1693 CentralPlaza Ladprao, Level 2, Unit 217 Phahonyothin Rd, Chatuchak Sub-district , Chatuchak District, Bangkok" ,storeLatitude : 37.755834 , storeLongitude : -122.416417 , storePhoneNumber : "062-875-9836")
-        let store2 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora ", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "7/222 Central Plaza Pinklao, Unit 106, Level 1 Boromratchonni Road, Arun-Amarin, Bangkoknoi, Bangkok 10700" , storeLatitude : 37.755834 , storeLongitude : -122.416417 , storePhoneNumber : "062-875-9836")
-        let store3 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.755834 , storeLongitude : -122.416417, storePhoneNumber : "062-875-9836")
-        let store4 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.755834 , storeLongitude : -122.416417, storePhoneNumber : "062-875-9836")
+        let store1 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora CentralPlaza Ladprao", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "1693 CentralPlaza Ladprao, Level 2, Unit 217 Phahonyothin Rd, Chatuchak Sub-district , Chatuchak District, Bangkok" ,storeLatitude : 37.755084 , storeLongitude : -122.416200 , storePhoneNumber : "062-875-9836")
+        let store2 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora ", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "7/222 Central Plaza Pinklao, Unit 106, Level 1 Boromratchonni Road, Arun-Amarin, Bangkoknoi, Bangkok 10700" , storeLatitude : 37.755435 , storeLongitude : -122.400100 , storePhoneNumber : "062-875-9836")
+        let store3 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.746833 , storeLongitude : -122.416412, storePhoneNumber : "062-875-9836")
+        let store4 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.778836 , storeLongitude : -122.416411, storePhoneNumber : "062-875-9836")
         
         return [store1 , store2 , store3 , store4]
     }
@@ -35,7 +35,7 @@ class NearByViewController: UIViewController   {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionViewLayout.minimumLineSpacing = 10
-        
+        mapCollectionView.delegate = self
         self.stores = self.createStoreArray()
         
         mapCollectionView.contentInset = UIEdgeInsets(top: 0.0, left: padding, bottom: 0.0, right: padding)
@@ -84,6 +84,27 @@ extension NearByViewController : UICollectionViewDelegate , UICollectionViewData
         let indexOfMajorCell = self.indexOfMajorCell()
         let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
         collectionViewLayout.collectionView!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView is UICollectionView {
+            let collectionViewCenter = CGPoint(x: mapCollectionView.bounds.size.width / 2 + mapCollectionView.contentOffset.x, y: mapCollectionView.bounds.size.height / 2 + mapCollectionView.contentOffset.y)
+            let centredCellIndexPath = mapCollectionView.indexPathForItem(at: collectionViewCenter)
+            
+            guard let path = centredCellIndexPath else {
+                // There is no cell in the center of collection view (you might want to think what you want to do in this case)
+                return
+            }
+            
+            if let selectedAnnotation = mapView.selectedAnnotations.first {
+                // Ignore if correspondent annotation is already selected
+                if selectedAnnotation.isEqual(self.mapView.annotations[path.row]) {
+                    self.mapView.selectAnnotation(self.mapView.annotations[path.row], animated: true)
+                }
+            }
+        }
     }
     
 }
