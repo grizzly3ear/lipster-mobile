b15f4405182d8aad49d842f58ee8442eaed8c4cb
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-09-29 14:13:27 +0700

    [UPDATE] link collectionView with pinAnnotation

--- lipster-mobile/controller/NearByViewController.swift
+++ lipster-mobile/controller/NearByViewController.swift
@@ -14,11 +14,12 @@ class NearByViewController: UIViewController   {
     
     var pinView : MKAnnotationView!
     
+    var index = 1
     var stores = [Store]()
     func createStoreArray() -> [Store] {
         let store1 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora CentralPlaza Ladprao", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "1693 CentralPlaza Ladprao, Level 2, Unit 217 Phahonyothin Rd, Chatuchak Sub-district , Chatuchak District, Bangkok" ,storeLatitude : 37.755084 , storeLongitude : -122.416200 , storePhoneNumber : "062-875-9836")
         let store2 : Store = Store(storeLogoImage: UIImage(named: "Sephora_black_logo")!, storeName: "Sephora ", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "7/222 Central Plaza Pinklao, Unit 106, Level 1 Boromratchonni Road, Arun-Amarin, Bangkoknoi, Bangkok 10700" , storeLatitude : 37.755435 , storeLongitude : -122.400100 , storePhoneNumber : "062-875-9836")
-        let store3 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.746833 , storeLongitude : -122.416412, storePhoneNumber : "062-875-9836")
+        let store3 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etudess House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.746833 , storeLongitude : -122.416412, storePhoneNumber : "062-875-9836")
         let store4 : Store = Store(storeLogoImage: UIImage(named: "nopic")!, storeName: "Etude House Central Plaza Rama 2", storeHours: "Mon - Sun  10AM-10PM", storeAddress: "L1, Central Plaza Rama 2, 128 Rama II Rd, Khwaeng Samae Dam, Samae Dum, Krung Thep Maha Nakhon 10150" , storeLatitude : 37.778836 , storeLongitude : -122.416411, storePhoneNumber : "062-875-9836")
         
         return [store1 , store2 , store3 , store4]
@@ -45,11 +46,8 @@ class NearByViewController: UIViewController   {
         initMapViewDelegate()
         if let coor = mapView.userLocation.location?.coordinate{
             mapView.setCenter(coor, animated: true)
-            
-            
-
         }
-        
+     //   mapCollectionView.scrollToItem(at:  , at: .top, animated: false)
     }
 }
 
@@ -87,27 +85,10 @@ extension NearByViewController : UICollectionViewDelegate , UICollectionViewData
         
     }
     
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView is UICollectionView {
-            let collectionViewCenter = CGPoint(x: mapCollectionView.bounds.size.width / 2 + mapCollectionView.contentOffset.x, y: mapCollectionView.bounds.size.height / 2 + mapCollectionView.contentOffset.y)
-            let centredCellIndexPath = mapCollectionView.indexPathForItem(at: collectionViewCenter)
-            
-            guard let path = centredCellIndexPath else {
-                // There is no cell in the center of collection view (you might want to think what you want to do in this case)
-                return
-            }
-            
-            if let selectedAnnotation = mapView.selectedAnnotations.first {
-                // Ignore if correspondent annotation is already selected
-                if selectedAnnotation.isEqual(self.mapView.annotations[path.row]) {
-                    self.mapView.selectAnnotation(self.mapView.annotations[path.row], animated: true)
-                }
-            }
-        }
-    }
-    
+   
 }
+    
+
 
 extension NearByViewController: CLLocationManagerDelegate {
     func initCoreLocationDelegate() {
@@ -127,6 +108,18 @@ extension NearByViewController: MKMapViewDelegate {
         mapView.isScrollEnabled = true
         
     }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView)
+    {
+        print("click")
+        if let annotationTitle = view.annotation?.title
+        {
+            let i = stores.firstIndex(where: { $0.name == annotationTitle })
+            mapCollectionView.scrollToItem(at: IndexPath(item: i!, section: 0), at: .centeredHorizontally, animated: true)
+            print("User tapped on annotation with title: \(annotationTitle!)")
+//              mapCollectionView.scrollToItem(at: IndexPath(row: 0, section: 0) , at: .top, animated: false)
+
+        }
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let locValue: CLLocationCoordinate2D = manager.location!.coordinate
@@ -135,20 +128,22 @@ extension NearByViewController: MKMapViewDelegate {
         let region = MKCoordinateRegion(center: locValue, span: span)
         mapView.setRegion(region, animated: true)
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = locValue
-        annotation.title = "Beauty Buffet"
-        mapView.addAnnotation(annotation)
-        print(locValue)
-        stores.forEach { (store) in
+        for (index, store) in stores.enumerated() {
             let coor = CLLocation(latitude: store.latitude, longitude: store.longitude).coordinate
             let storeAnnotation = MKPointAnnotation()
             
             storeAnnotation.coordinate = coor
             storeAnnotation.title = "\(store.name)"
-            mapView.addAnnotation(storeAnnotation)
             
+            mapView.addAnnotation(storeAnnotation)
         }
+        
+//        //mapCollectionView.tag = 1
+//        if storeAnnotation.title {
+//        } else {
+//            mapCollectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
+//
+//        }
         
     }
     //MARK: - MapKit
@@ -167,7 +162,8 @@ extension NearByViewController: MKMapViewDelegate {
         pinView.canShowCallout = true
         pinView.isDraggable = true
         pinView.image = UIImage(named: "pin_lipstick")
-        
+        pinView.tag = index
+        index += 1
         return pinView
         
     }
