f2b7487807262c09a1b0199c5923c925a2e2181e
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-10-15 21:39:28 +0700

    [UPDATE] update collectionViewCell of map

--- lipster-mobile/view/NearByCollectionViewCell.swift
+++ lipster-mobile/view/NearByCollectionViewCell.swift
@@ -12,16 +12,16 @@ class NearByCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var storeLogoImage: UIImageView!
     @IBOutlet weak var storeName: UILabel!
-    @IBOutlet weak var storehours: UILabel!
+
     @IBOutlet weak var storeAddress: UILabel!
-    @IBOutlet weak var storePhoneNumber: UILabel!
     
-    func setStore(store: Store) {
-        //storeLogoImage.sd_setImage(with: URL(string: store.storeLogoImage), placeholderImage: UIImage(named: "nopic"))
+    
+func setStore(store: Store) {
+
         storeLogoImage.sd_setImage(with: URL(string: store.image), placeholderImage: UIImage(named: "nopic"))
         storeName.text = store.name
-        storehours.text = store.hours
+        
         storeAddress.text = store.address
-        storePhoneNumber.text = store.phoneNumber
+        
     }
 }
