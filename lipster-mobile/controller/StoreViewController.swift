5093ddf75fb2b02ef234b9ab3e67b789eb68f5ad
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-10-15 20:29:31 +0700

    [UPDATE] update storeDetail mainStoryBoard

--- lipster-mobile/controller/StoreViewController.swift
+++ lipster-mobile/controller/StoreViewController.swift
@@ -9,18 +9,22 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var storeAddress: UILabel!
     @IBOutlet weak var storePhoneNumber: UILabel!
     @IBOutlet weak var titleNavigation: UINavigationItem!
+    @IBOutlet weak var blackView: UIView!
     
     var storeDetail: Store?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        blackView.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        
         storeName.text = storeDetail!.name
         storeImageView.sd_setImage(with: URL(string: (storeDetail?.image)!), placeholderImage: UIImage(named: "nopic")!)
-   
         storeHours.text = storeDetail!.hours
         storeAddress.text = storeDetail!.address
         storePhoneNumber.text = storeDetail?.phoneNumber
-       // navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-       // self.titleNavigation.title = store?.name
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.titleNavigation.title = storeDetail?.name
     }
     
     
