3022fadae01594e0b097a6ead8659f9a179e0f33
Unknown <natwara.jaratvithitpong@mail.kmutt.ac.th>
2019-11-01 20:19:56 +0700

    [UPDATE] add notification model

--- lipster-mobile/model/Notification.swift
+++ lipster-mobile/model/Notification.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class Notification {
     
+    var title : String
+    var body: String
+    var date : String
+    var destination : String
+    var image : String
+    
+    init(title: String, description: String , dateAndTime : String , destination : String , image : String) {
+        self.title = title
+        self.body = description
+        self.date = dateAndTime
+        self.destination = destination
+        self.image = image
+       
+    }
+    init(){
+        title = String()
+        body = String()
+        date = String()
+        destination = String()
+        image = String()
+        
+    }
 }
